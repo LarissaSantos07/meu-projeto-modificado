e4076147404fc19ec5c8c0add00ad16cdb4b69df
Larissa Santos <sjesus.larissa@gmail.com>
2024-12-08 20:14:31 -0300

    Adicionei um título (h1) acima do Header com um estilo simples: alinhado ao centro e com uma cor verde.

--- modulo11/template-bootstrap/src/App.js
+++ modulo11/template-bootstrap/src/App.js
@@ -4,8 +4,10 @@ import ListarTarefa from "./pages/tarefa/ListarTarefa";
 function App() {
   return (
     <div className="App">
+      <h1 style={{ textAlign: 'center', color: '#4caf50' }}>Minhas Tarefas</h1>
       <Header />
       <ListarTarefa />
+      <Greeting />
     </div>
   );
 }
