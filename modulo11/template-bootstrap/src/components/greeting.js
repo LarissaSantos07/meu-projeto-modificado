95e931b4a02772dbd68187c23c38595fe0b7884d
Larissa Santos <sjesus.larissa@gmail.com>
2024-12-08 20:16:10 -0300

    Criei um componente Greeting que exibe uma saudação e o incluí abaixo de ListarTarefa.

---
+++ modulo11/template-bootstrap/src/components/greeting.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Greeting() {
+  return <h2 style={{ color: '#3f51b5', textAlign: 'center' }}>Bem-vindo ao Gerenciador de Tarefas!</h2>;
+}
+
+export default Greeting;
